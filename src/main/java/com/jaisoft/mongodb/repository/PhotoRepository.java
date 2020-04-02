1dc74b80c18ec620db3c4cebde7fb3195e588a0d
Jaime Gómez Moraleda <jaime.gomez.moraleda@gmail.com>
2020-04-02 03:20:04 +0200

    first commit

---
+++ src/main/java/com/jaisoft/mongodb/repository/PhotoRepository.java
@@ -0,0 +1,11 @@
+package com.jaisoft.mongodb.repository;
+
+import com.jaisoft.mongodb.model.Photo;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PhotoRepository extends MongoRepository<Photo, String> {
+
+}
+
---
+++ src/main/java/com/jaisoft/mongodb/service/PhotoService.java
@@ -0,0 +1,33 @@
+package com.jaisoft.mongodb.service;
+
+import com.jaisoft.mongodb.model.Photo;
+import com.jaisoft.mongodb.repository.PhotoRepository;
+import org.bson.BsonBinarySubType;
+import org.bson.types.Binary;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+
+@Service
+public class PhotoService {
+
+    private PhotoRepository photoRepo;
+
+
+    public PhotoService(PhotoRepository photoRepo){}
+
+
+
+    public Photo getPhoto(String id) {
+        return photoRepo.findById(id).get();
+    }
+
+    public String addPhoto(String title, MultipartFile file) throws IOException {
+        Photo photo = new Photo(title);
+        photo.setImage(new Binary(BsonBinarySubType.BINARY, file.getBytes()));
+        photo = photoRepo.insert(photo);
+        return photo.getId();
+    }
+}
